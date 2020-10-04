09578ecb24c030ec05f3a951df8cfc46c1a6f24d
cristianosky <cristianvillalba1999@gmail.com>
2020-10-04 12:06:54 -0500

    ejercicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FormComponent } from './pages/form/form.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { ActualizarComponent } from './pages/actualizar/actualizar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: 'form/:id', component: ActualizarComponent      
+    },
+
+    {
+      path: 'forms', component: FormsComponent
+    },
+
+    {
+      path: '**', pathMatch: 'full', redirectTo: 'forms'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormComponent } from './pages/form/form.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { ActualizarComponent } from './pages/actualizar/actualizar.component';
+import { PasswordValidationDirective } from './validations/password-validation.directive';
+import { EdadValidateDirective } from './validations/edad-validate.directive';
+import { ConfirmarPassDirective } from './validations/confirmar-pass.directive';
+import { DisableCopyPasteDirective } from './disable-copy-paste.directive';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    FormsComponent,
+    ActualizarComponent,
+    PasswordValidationDirective,
+    EdadValidateDirective,
+    ConfirmarPassDirective,
+    DisableCopyPasteDirective,
   ],
+  entryComponents: [FormComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatIconModule  
   ],
-  providers: [],
+  providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
